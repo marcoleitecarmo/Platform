c0b1860e6729778682e7ebfc2f97c5894e0a9615
marcoleitecarmo <marcoleitecarmo@gmail.com>
2022-01-13 20:14:57 -0300

    aula 1 do jogo em pygame

--- obj.py
+++ obj.py
@@ -3,9 +3,14 @@ import pygame
 
 class Obj(pygame.sprite.Sprite):
 
-    def __init__(self, image,  *groups):
+    def __init__(self, image, x, y,  *groups):
         super().__init__(*groups)
 
         self.image = pygame.image.load(image)
+        self.rect = self.image.get_rect()
+        self.rect[0] = x
+        self.rect[1] = y
+
+
 
 
